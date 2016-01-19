cdd93c989bfa0d632bc43472c792a81ed5c7d42d
Bill Tyler <bill@tyler-labs.com>
2016-01-19 03:12:41 -0600

    first pass at roboclaw diagnistics service

--- src/nomad/nomad_drive.py
+++ src/nomad/nomad_drive.py
@@ -23,16 +23,105 @@ def drive_wheels(msg):
 	roboclaw.DriveM1(int(m1))
 	roboclaw.DriveM2(int(m2))
 
-def get_diag_info():
-	status = roboclaw.ReadError(0x80)
-	if status[0]==False:
-		print "GETSTATUS Failed"
-		return "GETSTATUS Failed"
-	else:
-		print repr(status[1])
-		return repr(status[1])
-
+def get_diag_info(input_string):
+	command = input_string.input
+	print command
 	
+	commandList = command.split(",")
+	print len(commandList)
+
+	returnmessage = "diagnostics info:"
+
+	for command in commandList:
+		print command
+
+		if(command == "firmware" or command == "all"): 
+			print "firmware"
+			#status = roboclaw.ReadVersion()
+			status = [True, "firmware"]
+
+			if status[0]==False:
+				print "Command Failed"
+			else:
+				print repr(status[1])
+
+			returnmessage += status[1] + ";"
+
+		if(command == "mainbat" or command == "all"): 
+			print "mainbat"
+			#status = roboclaw.ReadMainBattery()
+			status = [True, "mainbat"]
+
+			if status[0]==False:
+				print "Command Failed"
+			else:
+				print repr(status[1])
+
+			returnmessage += status[1] + ";"
+
+		if(command == "logicbat" or command == "all"): 
+			print "logicbat"
+			#status = roboclaw.ReadLogicBattery()
+			status = [True, "logicbat"]
+
+			if status[0]==False:
+				print "Command Failed"
+			else:
+				print repr(status[1])
+
+			returnmessage += status[1] + ";"
+
+		if(command == "temp" or command == "all"): 
+			print "temp"
+			#status = roboclaw.ReadTemperature()
+			status = [True, "temp"]
+
+			if status[0]==False:
+				print "Command Failed"
+			else:
+				print repr(status[1])
+
+			returnmessage += status[1] + ";"
+
+		if(command == "status" or command == "all"): 
+			print "status"
+			#status = roboclaw.ReadStatus()
+			status = [True, "status"]
+
+			if status[0]==False:
+				print "Command Failed"
+			else:
+				print repr(status[1])
+
+			returnmessage += status[1] + ";"
+
+		if(command == "config" or command == "all"): 
+			print "config"
+			#status = roboclaw.ReadConfig()
+			status = [True, "config"]
+
+			if status[0]==False:
+				print "Command Failed"
+			else:
+				print repr(status[1])
+
+			returnmessage += status[1] + ";"
+
+		if(command == "error" or command == "all"): 
+			print "error"
+			#status = roboclaw.ReadError(0x80)
+			status = [True, "error"]
+
+			if status[0]==False:
+				print "Command Failed"
+			else:
+				print repr(status[1])
+
+			returnmessage += status[1] + ";"
+
+	return returnmessage
+
+
 rospy.init_node('barb_drive')
 sub = rospy.Subscriber('cmd_vel', Twist, drive_wheels)
 service = rospy.Service('RoboclawDiagnostics', RoboclawDiagnostics, get_diag_info)
